b8baa9bf256e5e8459af5eb133df5314160df221
PontusNorrby <pontus.norrby@hotmail.se>
2020-11-12 12:06:40 +0100

    First editions of scripts and some insert statements.

---
+++ SQL/Insert/Testinsert.sql
@@ -0,0 +1,8 @@
+//Inserts a customer named Lars Sven
+INSERT INTO CUSTOMER (uname,psword,fname,sname,phone,email,sex,birthdate)
+VALUES ("customer1","firstcustomer","Lars","Sven",0761234567,"customer1@mail.new","Man", "1900-01-01");
+
+
+//Inserts a order at a Lars our only customer.
+INSERT INTO ORDERS(custid,orderdate,cost)
+VALUES((SELECT id FROM CUSTOMER WHERE fname = "Lars"),"2020-01-01",241421);
--- SQL/Table/customerscript.sql
+++ SQL/Table/customerscript.sql
@@ -1,11 +1,11 @@
-CREATE OR ALTER TABLE CUSTOMER (
+CREATE TABLE CUSTOMER (
   id int NOT NULL AUTO_INCREMENT,
   uname varchar(15) DEFAULT NULL,
-  password varchar,
+  psword varchar(255),
   fname varchar(15),
-  sname varchar(15)
+  sname varchar(15),
   phone int,
-  email varchar,
+  email varchar(255),
   admin bit DEFAULT 0,
   sex varchar(5),
   birthdate date,
