736b2975aa7b786fcfa67c92b5abb1a1e3f1549a
antstr-8 <antstr-8@student.ltu.se>
2020-11-23 16:55:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,45 @@
 
   require_once "php/config.php";
 
+  if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
+      if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+        $prodid = $_POST['prodid'];
+        $quantity = $_POST['quantity'];
+
+        $sql2 = "SELECT quantity FROM cart WHERE custid = :custid AND prodid = :prodid";
+
+        if($stmt2 = $pdo->prepare($sql2)){
+          $stmt2->bindParam(":custid", $_SESSION['id'], PDO::PARAM_STR);
+          $stmt2->bindParam(":prodid", $prodid, PDO::PARAM_STR);
+
+          if($stmt2->execute()){
+              if($stmt2->rowCount() >= 1){
+                $res = $stmt2->fetch();
+                $quantity = $res['quantity'] + $quantity;
+
+                $sql = "UPDATE cart SET quantity = :quantity
+                WHERE prodid = :prodid and custid = :custid";
+              }
+              else{
+                $sql = "INSERT INTO cart (custid, prodid, quantity)
+                VALUES (:custid, :prodid, :quantity)";
+              }
+            }
+          }
+
+        if($stmt = $pdo->prepare($sql)){
+          $stmt->bindParam(":custid", $_SESSION['id'], PDO::PARAM_STR);
+          $stmt->bindParam(":prodid", $prodid , PDO::PARAM_STR);
+          $stmt->bindParam(":quantity", $quantity , PDO::PARAM_STR);
+
+          if($stmt->execute()){
+
+          }
+        }
+      }
+  }
+
  ?>
 <!DOCTYPE html>
 <html>
@@ -17,7 +56,7 @@
     <div class="logBox">
     <?php
     if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-      echo "Welcome " . $_SESSION["username"];
+      echo "Welcome <a href='php/profile.php'>" . $_SESSION["username"] . "</a>";
       echo '<style type="text/css">
               .login {
                 display:none;
@@ -34,6 +73,7 @@
     ?>
      <a class="login" href="php/login.php">Login</a>
      <a class="login" href="php/register.php">Sign up</a>
+     <a class="logout" href="php/cart.php">Cart</a>
      <a class="logout" href="php/logout.php">Sign out</a>
    </div>
   </div>
@@ -50,29 +90,39 @@
               $stht = $pdo->prepare("SELECT color,stock,url,price FROM prodinfo WHERE id=$row[id]");
               $stht->execute();
               $res = $stht->fetchAll();
-              foreach($res as $row2){
-                echo "<div class='product_row'>";
-                echo "<div class='pictures'>";
-                echo "<img src=" . $row2['url'] . " style='max-width: 100%;max-height: 100%;'>";
-                /*echo "testbild";*/
-                echo "</div>";
-                echo "<div class='description'>";
-                echo $row['description'];
-                echo "</div>";
-                echo "<div class='price'>";
-                 echo "<div class='artname'>";
-                   echo $row['name'];
-                 echo "</div>";
-                echo $row2['price'];
-                echo "<br>";
-                echo $row2['stock'];
-                echo "<br>";
-                echo $row2['color'];
-                echo "</div>";
-                echo "</div>";
-             }
+              foreach($res as $row2){ ?>
+
+
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+
+
+                <div class='product_row'>
+                  <div class='pictures'>
+                    <img src="<?php echo $row2['url'];?>" style='max-width: 100%; max-height: 100%;'>
+                <!-- echo "testbild"-->
+                   </div>
+                   <div class='description'>
+                     <?php echo $row['description']; ?>
+                   </div>
+                   <div class='price'>
+                   <div class='artname'>
+                    <?php echo $row['name']; ?>
+                   </div>
+                <?php echo $row2['price']; ?>
+                <br>
+                <?php echo $row2['stock'];?>
+                <br>
+                <?php echo $row2['color'];?>
+                </div>
+                </div>
+                  <input type="number" name="quantity" value="1">
+                  <input type="hidden" name="prodid" value="<?php echo $row['id'];?>">
+                  <input type="submit" value="Add to cart">
+                </form>
+              <?php
             }
-          ?>
+            }
+           ?>
        </div>
        <br>
   <!--<p>
