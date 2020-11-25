61b942dc4c3a5bd0f3608d5a00a4beb5e969f3d5
antstr-8 <antstr-8@student.ltu.se>
2020-11-25 11:40:51 +0100

    Update cart.php
    Added so that the user can remove things from the cart

--- php/cart.php
+++ php/cart.php
@@ -2,7 +2,49 @@
 
   session_start();
   require_once "config.php";
+  if($_SERVER["REQUEST_METHOD"] == "POST"){
+    if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
 
+      $id = $_SESSION['id'];
+      $prodid = $_POST['prodid'];
+      $quantity = $_POST['quantity'];
+
+      $sqlUpdate = "SELECT * FROM cart WHERE custid = :custid AND prodid = :prodid";
+      if($updateStmt = $pdo->prepare($sqlUpdate)){
+        $updateStmt->bindParam(":prodid", $prodid, PDO::PARAM_STR);
+        $updateStmt->bindParam(":custid", $id, PDO::PARAM_STR);
+
+        if($updateStmt->execute()){
+          if($updateStmt->rowCount() == 1){
+            $res = $updateStmt->fetch();
+            if($res['quantity'] >= $quantity){
+              $updatedQuantity =  $res['quantity'] - $quantity;
+              if($updatedQuantity == 0){
+                $sqlUpdate2 = "DELETE FROM cart WHERE custid = :custid
+                AND prodid = :prodid";
+                if($updateStmt2 = $pdo->prepare($sqlUpdate2)){
+                  $updateStmt2->bindParam(":custid", $id, PDO::PARAM_STR);
+                  $updateStmt2->bindParam(":prodid", $prodid , PDO::PARAM_STR);
+                }
+              }
+              elseif($updatedQuantity > 0){
+                $sqlUpdate2 = "UPDATE cart SET quantity = :quantity
+                WHERE prodid = :prodid AND custid = :custid";
+                if($updateStmt2 = $pdo->prepare($sqlUpdate2)){
+                  $updateStmt2->bindParam(":custid", $id, PDO::PARAM_STR);
+                  $updateStmt2->bindParam(":prodid", $prodid , PDO::PARAM_STR);
+                  $updateStmt2->bindParam(":quantity", $updatedQuantity , PDO::PARAM_STR);
+                }
+              }
+              $updateStmt2->execute();
+            }
+          }
+        }
+      }
+    }
+      unset($updateStmt);
+      unset($updateStmt2);
+  }
  ?>
 
 
@@ -31,33 +73,46 @@
                   $newstmt = $pdo->prepare("SELECT name,description FROM prodcat WHERE id=$row2[id]");
                   $newstmt->execute();
                   $newres = $newstmt->fetchAll();
-                  foreach($newres as $row3){
-                    echo "<div class='product_row'>";
-                    echo "<div class='pictures'>";
-                    echo "<img src=" . $row2['url'] . " style='max-width: 100%;max-height: 100%;'>";
-                    /*echo "testbild";*/
-                    echo "</div>";
-                    echo "<div class='description'>";
-                    echo $row3['description'];
-                    echo "</div>";
-                    echo "<div class='price'>";
-                     echo "<div class='artname'>";
-                       echo $row3['name'];
-                     echo "</div>";
-                    echo $row2['price'];
-                    echo "<br>";
-                    echo $row['quantity'];
-                    echo "<br>";
-                    echo $row2['color'];
-                    echo "</div>";
-                    echo "</div>";
+                  foreach($newres as $row3){ ?>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+
+
+                    <div class='product_row'>
+                      <div class='pictures'>
+                        <img src="<?php echo $row2['url'];?>" style='max-width: 100%; max-height: 100%;'>
+                    <!-- echo "testbild"-->
+                       </div>
+                       <div class='description'>
+                         <?php echo $row3['description']; ?>
+                       </div>
+                       <div class='price'>
+                       <div class='artname'>
+                        <?php echo $row3['name']; ?>
+                       </div>
+                    <?php echo $row2['price']; ?>
+                    <br>
+                    <?php echo $row['quantity'];?>
+                    <br>
+                    <?php echo $row2['color'];?>
+                    </div>
+                    </div>
+                      <input type="number" name="quantity" value="1">
+                      <input type="hidden" name="prodid" value="<?php echo $row2['id'];?>">
+                      <input type="submit" value="Remove from cart">
+                    </form>
+
+                    <?php
                }
               }
             }
+            unset($stmt);
+            unset($stht);
+            unset($newstmt);
         }else{
           header("location: /../index.php");
         }
           ?>
+          <a href="/../index.php">Home</a>
     </div>
   </body>
 </html>
