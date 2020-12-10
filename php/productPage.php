7096a7f45ca1d3b18795333d8ce0b4abd5554ddd
PontusNorrby <pontus.norrby@hotmail.se>
2020-12-10 17:09:41 +0100

    Added some small stuff

--- php/productPage.php
+++ php/productPage.php
@@ -43,10 +43,48 @@
  <!DOCTYPE html>
  <html lang="en" dir="ltr">
    <head>
+       <link rel="stylesheet" href="/css/style.css">
      <meta charset="utf-8">
-     <title></title>
+     <title>Product waow</title>
    </head>
    <body>
+     <!--HEADER STARTS HERE-->
+     <div class="header">
+       <div class="logBox">
+       <?php
+       if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+         echo "Welcome <a href='php/profile.php'>" . $_SESSION["username"] . "</a>";
+         echo '<style type="text/css">
+                 .login {
+                   display:none;
+                 }
+               </style>';
+         if($_SESSION["admin"] == 1){
+           echo '<style type="text/css">
+                   .admin {
+                     display: inline;
+                   }
+                 </style>';
+         }
+       }
+
+       else{
+         echo '<style type="text/css">
+                 .logout {
+                   display:none;
+                 }
+               </style>';
+       }
+       ?>
+        <a class="login" href="php/login.php">Login</a>
+        <a class="login" href="php/register.php">Sign up</a>
+        <a class="logout" href="php/cart.php">Cart</a>
+        <a class="logout" href="php/logout.php">Sign out</a>
+        <a class="admin" href="php/admin.php">Admin</a>
+      </div>
+     </div>
+
+     <!--HEADER ENDS HERE-->
      <p>Productname: <?php echo $res2['name']?></p>
      <p>Description: <br> <?php echo $res2['description']?></p>
      <p>Price: <?php echo $res1['price']?></p>
